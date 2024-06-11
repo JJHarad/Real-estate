869ffa2ef0cf64481c4f81b05996a593bbb65af4
JJHarad <jidnyaharad11@gmail.com>
2024-06-12 00:04:10 +0530

    create user update api

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,28 +1,26 @@
+// api/models/user.model.js
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-
-    username:{
-       type:String,
-       required:true,
-       unique:true, 
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true, 
-     },
-     password:{
-        type:String,
-        required:true, 
-     },
-     avatar:{
-      type:String,
-      default: "https://th.bing.com/th/id/OIP.lcdOc6CAIpbvYx3XHfoJ0gAAAA?rs=1&pid=ImgDetMain"
-     }
-},
-{timestamps: true}
-);
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: 'https://example.com/default-avatar.png',
+    },
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
