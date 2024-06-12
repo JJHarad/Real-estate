9577c94b40416ddc019e286d2262a58685c2c47e
JJHarad <jidnyaharad11@gmail.com>
2024-06-12 05:52:16 +0530

    listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -26,6 +27,8 @@ app.use('/api/user', userRouter);
 
 // Mount authRouter at /api/auth
 app.use('/api/auth', authRouter);
+
+app.use('/api/listing', listingRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
