92952a0994c9ec7df1d9e133438c807779a0c97d
JJHarad <jidnyaharad11@gmail.com>
2024-09-08 22:16:10 +0530

    build and deploy

--- api/index.js
+++ api/index.js
@@ -3,44 +3,49 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
-
-// Load environment variables from .env file
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
 
 const app = express();
+
 app.use(express.json());
 
 app.use(cookieParser());
-// Mount userRouter at /api/user
-app.use('/api/user', userRouter);
-
-// Mount authRouter at /api/auth
-app.use('/api/auth', authRouter);
-
-app.use('/api/listing', listingRouter);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
-});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
