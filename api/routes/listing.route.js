467855199915971c3feebc6d619a5bb39629d19e
JJHarad <jidnyaharad11@gmail.com>
2024-09-08 07:53:50 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing ,updateListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 //import jwt from 'jsonwebtoken';
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);
 router.delete( '/delete/:id', verifyToken,  deleteListing );
 //delete wala route
+router.post('/update/:id', verifyToken, updateListing);
+// router.get('/get/:id', getListing);
+// router.get('/get', getListings);
 
 export default router;
