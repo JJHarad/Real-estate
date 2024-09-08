8033b8f4b77d48ede47b8ae3d81df03387ed1cd1
JJHarad <jidnyaharad11@gmail.com>
2024-09-08 09:15:53 +0530

    done listing page

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing ,updateListing} from '../controllers/listing.controller.js';
+import { createListing, deleteListing ,updateListing,getListing,getListings} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 //import jwt from 'jsonwebtoken';
 const router = express.Router();
@@ -8,7 +8,7 @@ router.post('/create', verifyToken, createListing);
 router.delete( '/delete/:id', verifyToken,  deleteListing );
 //delete wala route
 router.post('/update/:id', verifyToken, updateListing);
-// router.get('/get/:id', getListing);
-// router.get('/get', getListings);
+ router.get('/get/:id', getListing);
+ router.get('/get', getListings);
 
 export default router;
